3ac91cbd8453285c5acc01965cbe6abf1b5b9513
dankukennedy <dankukennedy@email.com>
2024-05-28 13:04:02 +0000

    restructuring the nav files

---
+++ portal/dashboard1.php
@@ -0,0 +1,23 @@
+       <!-- including header -->
+        <?php  include "includes/header1.php"   ?>
+
+            <!-- Sidebar  -->
+            <?php include "includes/nav1.php" ?>
+
+            <!-- end sidebar -->
+            
+            <!-- right content -->
+            <div id="content">
+
+               <!-- topbar -->
+              <?php include "includes/topbar1.php" ?>
+
+               <!-- end topbar -->
+
+               <!-- dashboard inner -->
+               <?php include "includes/dashboard1.php" ?>
+
+               <!-- end dashboard inner -->
+            </div>
+
+            <?php include "includes/footer1.php" ?>
